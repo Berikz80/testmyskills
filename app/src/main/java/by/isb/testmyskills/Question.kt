82371016c042e3c5cef63cfd76af524bc9bb74f9
Igor <berikz@gmail.com>
2021-04-29 19:54:44 +0300

    add readfromfile

---
+++ app/src/main/java/by/isb/testmyskills/Question.kt
@@ -0,0 +1,14 @@
+package by.isb.testmyskills
+
+class Question(
+    var complexity: Int,
+    var question: String,
+    var answerRight: String,
+    var answer1: String,
+    var answer2: String,
+    var answer3: String
+) {
+
+
+
+}
